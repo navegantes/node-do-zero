8e781a26f54ee734c857694ed0dd966408c7bf2c
navegantes <raphaelnavegantes@gmail.com>
2023-09-14 19:37:42 -0300

    add host config

--- server.js
+++ server.js
@@ -64,4 +64,7 @@ server.delete('/videos/:id', async (req, reply) => {
   return reply.status(204).send()
 })
 
-server.listen({ port: process.env.PORT ?? 3333 })
+server.listen({
+  host: '0.0.0.0',
+  port: process.env.PORT ?? 3333
+})
